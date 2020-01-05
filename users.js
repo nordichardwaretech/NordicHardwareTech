f6b22abee1763a5fedfc53de3553d45cdcea7b19
nordichardwaretech <58564351+nordichardwaretech@users.noreply.github.com>
2020-01-05 18:11:56 +0100

    Added files via upload
    This is all the files for the website

---
+++ users.js
@@ -0,0 +1,58 @@
+var objPeople = [
+	{ // Object @ 2 index
+		username: "Youpcaso",
+		password: "hrtnerd"
+	},
+	{ // Object @ 4 index
+		username: "stmxjo",
+		password: "verysecurepassword"
+	},
+	{ // Object @ 5 index
+		username: "Major",
+		password: "lofin"
+	},
+	{ // Object @ 6 index
+		username: "Yurefo",
+		password: "83hrje"
+	},
+	{ // Object @ 7 index
+		username: "StrodIan",
+		password: "Returned"
+	},
+	{ // Object @ 8 index
+		username: "DionECac",
+		password: "LoinCeac"
+	},
+	{ // Object @ 9 index
+		username: "indexorthert",
+		password: "Lambatross"
+	},
+	{
+		username: "NordicHardwareTech",
+		password: "academy"
+	},
+	{
+		username: "youpcaso",
+		password: "Oupsaco"
+	},
+	{
+		username: "stmxjo",
+		password: "supersecurepassword"
+	},
+	{
+		username: "TimTimmen",
+		password: "xcd12tghT"
+	},
+	{
+		username: "CarlHedemalm",
+		password: "HoleFukAndBangDingOw"
+	},
+	{
+		username: "Major",
+		password: "Uterbame"
+	},
+	{
+		username: "carlhedemalm",
+		password: "ttttyyyy84"
+	},
+]
